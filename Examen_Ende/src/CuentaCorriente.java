f6b751ced26908ab517e48ef47a7fda81f246dfa
user <user@DESKTOP-B366R1N>
2026-03-06 13:00:22 +0100

    Creación de la clase Cuenta Corriente.

---
+++ Examen_Ende/src/CuentaCorriente.java
@@ -0,0 +1,18 @@
+
+public class CuentaCorriente extends CuentaBancaria{
+
+	private int comision;
+	private int interes;
+
+	public CuentaCorriente(String titular, double saldoInicial,int comision ) {
+		super(titular, saldoInicial);
+		this.comision = comision;
+	}
+	
+	public int CalcularInteres(int comision) {
+		int interes = comision--;
+		return interes;
+	}
+
+	
+}
