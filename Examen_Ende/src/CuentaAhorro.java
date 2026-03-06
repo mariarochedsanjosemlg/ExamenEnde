4b34ca56cd4398e1bc85d81780fe80f65d8d050b
user <user@DESKTOP-B366R1N>
2026-03-06 14:08:16 +0100

    Documentación Clase Cuenta Ahorro

--- Examen_Ende/src/CuentaAhorro.java
+++ Examen_Ende/src/CuentaAhorro.java
@@ -1,14 +1,34 @@
-
+/*
+ * Clase Cuenta Ahorro, hija de clase Cuenta Bancaria
+ * @Author María Roche Durán
+ * @version 1.00
+ */
 public class CuentaAhorro extends CuentaBancaria{
-
+/*
+ * Variable tasa de interés propia de la clase
+ */
 	private double tasaInteres;
+	/*
+	 * Variable de interés que hará falta más adelante
+	 */
 	private double interes;
 
+	/*
+	 * Clase cuenta Ahorro
+	 * @param String titular
+	 * @param double SaldoInicial
+	 * @param double tasaInteres
+	 */
 	public CuentaAhorro(String titular, double saldoInicial,double tasaInteres ) {
 		super(titular, saldoInicial);
 		this.tasaInteres = tasaInteres;
 	}
 	
+	/*
+	 * Método para calcular interes.
+	 * @param tasa Interes
+	 * @return interes
+	 */
 	public double CalcularInteres(double tasaInteres) {
 		double interes = saldo * tasaInteres;
 		return interes;
