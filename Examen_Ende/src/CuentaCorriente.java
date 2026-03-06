6dca02bb4975ed1e27c606a6835800029ffe2895
user <user@DESKTOP-B366R1N>
2026-03-06 14:11:47 +0100

    Documentación Clase Cuenta Corriente

--- Examen_Ende/src/CuentaCorriente.java
+++ Examen_Ende/src/CuentaCorriente.java
@@ -1,14 +1,35 @@
-
+/*
+ * Clase Cuenta corriente, hija de Cuenta Bancaria, cuenta con sus propios atributos comision e interes
+ * @author María Roche Durán
+ * @version 1.0.0
+ */
 public class CuentaCorriente extends CuentaBancaria{
 
+	/*
+	 * Variable int comisión.
+	 */
 	private int comision;
+	/*
+	 * Variable int interes.
+	 */
 	private int interes;
 
+	/*
+	 * Clase cuenta corriente.
+	 * @param String titular.
+	 * @param double saldo Inicial.
+	 * @param int comision.
+	 */
 	public CuentaCorriente(String titular, double saldoInicial,int comision ) {
 		super(titular, saldoInicial);
 		this.comision = comision;
 	}
 	
+	/*
+	 * Método para calcular interes.
+	 * @param tasa Interes
+	 * @return interes
+	 */
 	public int CalcularInteres(int comision) {
 		int interes = -comision;
 		return interes;
